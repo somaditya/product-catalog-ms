d1ecd65fa03f0017dab27841af0ab756b4a51254
Somaditya Basak <somaditya.b@gmail.com>
2022-10-01 23:03:43 +0530

    product catalog service init

---
+++ product-info-service/src/main/java/com/amdocs/productinfoservice/ProductInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.productinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInfoServiceApplication.class, args);
+	}
+
+}
